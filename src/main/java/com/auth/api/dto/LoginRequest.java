fcacf4051dfec4773d3bcc1dd3122921284fce60
Hachim Hassani <hachim.hassani@gmail.com>
2025-03-04 14:16:10 +0100

    bkp

---
+++ src/main/java/com/auth/api/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.auth.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String username;
+    
+    @NotBlank
+    private String password;
+}
---
+++ src/main/java/com/auth/api/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.auth.api.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.auth.api.model.User;
+import com.auth.api.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé avec le nom d'utilisateur: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
